46687c2fd3f579bd54624a47ff362e793d5e8eee
Алексей Подъяблонский <lelikpodyablonskiy@yandex.ru>
2022-05-05 17:17:26 +0300

    поиск ошибки

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,31 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 
-def group_posts(request):
-    return HttpResponse('Группы')
+def group_list(request):
+    template = 'posts/group_list.html'
+    title = 'Yatube'
+    content = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'content': content
+    }
+    return render(request, template, context)
 
-
-def group_filter(request, slug):
-    return HttpResponse(f'Подборка {slug}')
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': 'название группы',
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
